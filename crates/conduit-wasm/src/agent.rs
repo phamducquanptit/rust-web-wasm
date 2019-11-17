6b2b0ebcc2a883674e4d0e5131fc0d1aecc1d0a0
Quan <phamducquanptithcm@gmail.com>
2019-11-17 16:48:43 +0700

    add main.rs

---
+++ crates/conduit-wasm/src/agent.rs
@@ -0,0 +1,228 @@
+#![allow(dead_code)]
+
+use log::debug;
+use serde::{Deserialize, Serialize};
+use serde_json;
+use yew::callback::Callback;
+use yew::format::{Json, Nothing, Text};
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
+
+use crate::error::Error;
+use crate::types::*;
+
+const API_ROOT: &str = "https://conduit.productionready.io/api";
+
+#[derive(Default, Debug)]
+struct Requests {
+    fetch: FetchService,
+}
+
+impl Requests {
+    fn new() -> Self {
+        Self {
+            fetch: FetchService::new(),
+        }
+    }
+
+    fn builder<B, T>(
+        &mut self,
+        method: &str,
+        url: String,
+        body: B,
+        callback: Callback<Result<T, Error>>,
+    ) -> FetchTask
+    where
+        for<'de> T: Deserialize<'de> + 'static + std::fmt::Debug,
+        B: Into<Text> + std::fmt::Debug,
+    {
+        let url = format!("{}{}", API_ROOT, url);
+        let handler = move |response: Response<Text>| {
+            if let (meta, Ok(data)) = response.into_parts() {
+                debug!("Response: {:?}", data);
+                if meta.status.is_success() {
+                    let data: Result<T, _> = serde_json::from_str(&data);
+                    if let Ok(data) = data {
+                        callback.emit(Ok(data))
+                    } else {
+                        callback.emit(Err(Error::DeserializeError))
+                    }
+                } else {
+                    match meta.status.as_u16() {
+                        401 => callback.emit(Err(Error::Unauthorized)),
+                        403 => callback.emit(Err(Error::Forbidden)),
+                        404 => callback.emit(Err(Error::NotFound)),
+                        500 => callback.emit(Err(Error::InternalServerError)),
+                        422 => {
+                            let data: Result<ErrorInfo, _> = serde_json::from_str(&data);
+                            if let Ok(data) = data {
+                                callback.emit(Err(Error::UnprocessableEntity(data)))
+                            } else {
+                                callback.emit(Err(Error::DeserializeError))
+                            }
+                        }
+                        _ => callback.emit(Err(Error::RequestError)),
+                    }
+                }
+            } else {
+                callback.emit(Err(Error::RequestError))
+            }
+        };
+        let request = Request::builder()
+            .method(method)
+            .uri(url.as_str())
+            .header("Content-Type", "application/json")
+            .body(body)
+            .unwrap();
+        debug!("Request: {:?}", request);
+        self.fetch.fetch(request, handler.into())
+    }
+
+    fn delete<T>(&mut self, url: String, callback: Callback<Result<T, Error>>) -> FetchTask
+    where
+        for<'de> T: Deserialize<'de> + 'static + std::fmt::Debug,
+    {
+        self.builder("DELETE", url, Nothing, callback)
+    }
+
+    fn get<T>(&mut self, url: String, callback: Callback<Result<T, Error>>) -> FetchTask
+    where
+        for<'de> T: Deserialize<'de> + 'static + std::fmt::Debug,
+    {
+        self.builder("GET", url, Nothing, callback)
+    }
+
+    fn post<B, T>(
+        &mut self,
+        url: String,
+        body: B,
+        callback: Callback<Result<T, Error>>,
+    ) -> FetchTask
+    where
+        for<'de> T: Deserialize<'de> + 'static + std::fmt::Debug,
+        B: Serialize,
+    {
+        let body: Text = Json(&body).into();
+        self.builder("POST", url, body, callback)
+    }
+
+    fn put<B, T>(&mut self, url: String, body: B, callback: Callback<Result<T, Error>>) -> FetchTask
+    where
+        for<'de> T: Deserialize<'de> + 'static + std::fmt::Debug,
+        B: Serialize,
+    {
+        let body: Text = Json(&body).into();
+        self.builder("PUT", url, body, callback)
+    }
+}
+
+fn limit(count: u32, p: u32) -> String {
+    let offset = if p > 0 { p * count } else { 0 };
+    format!("limit={}&offset={}", count, offset)
+}
+
+#[derive(Default, Debug)]
+pub struct Articles {
+    requests: Requests,
+}
+
+impl Articles {
+    pub fn new() -> Self {
+        Self {
+            requests: Requests::new(),
+        }
+    }
+
+    pub fn all(
+        &mut self,
+        page: u32,
+        callback: Callback<Result<ArticleListInfo, Error>>,
+    ) -> FetchTask {
+        self.requests
+            .get::<ArticleListInfo>(format!("/articles?{}", limit(10, page)), callback)
+    }
+
+    pub fn by_author(
+        &mut self,
+        author: String,
+        page: u32,
+        callback: Callback<Result<ArticleListInfo, Error>>,
+    ) -> FetchTask {
+        self.requests.get::<ArticleListInfo>(
+            format!("/articles?author={}&{}", author, limit(10, page)),
+            callback,
+        )
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct Tags {
+    requests: Requests,
+}
+
+impl Tags {
+    pub fn new() -> Self {
+        Self {
+            requests: Requests::new(),
+        }
+    }
+
+    pub fn get_all(&mut self, callback: Callback<Result<TagListInfo, Error>>) -> FetchTask {
+        self.requests
+            .get::<TagListInfo>("/tags".to_string(), callback)
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct Auth {
+    requests: Requests,
+}
+
+impl Auth {
+    pub fn new() -> Self {
+        Self {
+            requests: Requests::new(),
+        }
+    }
+
+    pub fn current(&mut self, callback: Callback<Result<UserInfoWrapper, Error>>) -> FetchTask {
+        self.requests
+            .get::<UserInfoWrapper>("/user".to_string(), callback)
+    }
+
+    pub fn login(
+        &mut self,
+        login_info: LoginInfoWrapper,
+        callback: Callback<Result<UserInfoWrapper, Error>>,
+    ) -> FetchTask {
+        self.requests.post::<LoginInfoWrapper, UserInfoWrapper>(
+            "/users/login".to_string(),
+            login_info,
+            callback,
+        )
+    }
+
+    pub fn register(
+        &mut self,
+        register_info: RegisterInfoWrapper,
+        callback: Callback<Result<UserInfoWrapper, Error>>,
+    ) -> FetchTask {
+        self.requests.post::<RegisterInfoWrapper, UserInfoWrapper>(
+            "/users".to_string(),
+            register_info,
+            callback,
+        )
+    }
+
+    pub fn save(
+        &mut self,
+        user_update_info: UserUpdateInfoWrapper,
+        callback: Callback<Result<UserInfoWrapper, Error>>,
+    ) -> FetchTask {
+        self.requests
+            .post::<UserUpdateInfoWrapper, UserInfoWrapper>(
+                "/user".to_string(),
+                user_update_info,
+                callback,
+            )
+    }
+}
